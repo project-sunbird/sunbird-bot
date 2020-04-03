abec4e75ce4e6f23870c65be514aa19b5fa349fa
waibhav chandra <waibhav.chandra@tarento.com>
2020-04-03 10:42:48 +0530

    telegram integration code

--- router/config/config.js
+++ router/config/config.js
@@ -8,6 +8,7 @@ config.SOCKET_HTTP_PORT              = 4005
 config.REST_HTTP_PORT                = 4000
 config.SOCKET_HTTPS_PORT             = 4001
 config.REST_HTTPS_PORT               = 8443
+config.TELEGRAM_HTTPS_PORT           = 4009
 
 config.HTTPS_PATH_KEY                = ''
 config.HTTPS_PATH_CERT               = ''
@@ -17,6 +18,8 @@ config.HTTPS_PATH_CA                 = ''
 config.ELASTIC_HOST                  = 'http://<user>:<password><IP>:<Port>'
 config.ELASTIC_INDEX_NAME            = 'indx_name'
 config.ELASTIC_INDEX_TYPE            = 'indx_type'
+
+config.TELEGRAM_BOT_ENDPOINT         = 'https://api.telegram.org/bot<bot_secret_key>/sendMessage'
 
 
 module.exports = config;
