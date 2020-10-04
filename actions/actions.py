4b6a4821dbb89bb65049852d3072ca52680980be
ajinkyaghodake123 <ajinkya.ghodake@tarento.com>
2020-10-04 23:16:53 +0530

    docker changes

--- actions/actions.py
+++ actions/actions.py
@@ -91,8 +91,6 @@ class FrameworkApi:
    def fetch_channel_data(self, board):
       obj = {}
       obj['time'] = int(round(time.time() * 1000))
-      # =call the framework api
-      # counter = 0
       print("call the api-channel",self.channel_counter+1)
       channel_res = requests.get(
             "https://staging.ntp.net.in/api/channel/v1/read/0126632859575746566") 
@@ -128,8 +126,12 @@ class FrameworkApi:
       # last_time = channel_data[board]['time']
      
       time_compare = last_time+self.ttl
+      print("time_compare-->",time_compare)
+      print("self.ttl-->",self.ttl)
       current_time = int(round(time.time() * 1000))
-      if current_time < current_time:
+      print("current_time-->",current_time)
+      if time_compare < current_time:
+         print("inside time_compare if")
          if bool(board):
             self.fetch_channel_data(board)
          else:
@@ -137,6 +139,7 @@ class FrameworkApi:
 
          #  self.fetch_channel_data(board)
       if bool(board):
+         print("inside time if")
          return self.channel_data[board]['payload']
       else : 
          return self.framework_data[board_identifier]['payload']
@@ -166,6 +169,7 @@ class ActionContentForm(FormAction):
         print("a.channel_data-->",a.channel_data)
 
         if not bool(a.channel_data):
+           print("calling a.fetch_channel_data(value)")
            channel_response = a.fetch_channel_data(value)
            print("method1-->",channel_response)
         else:
@@ -188,12 +192,12 @@ class ActionContentForm(FormAction):
 
         res_json = channel_response.json()
 
-        api_matching_board = self.get_board_api_mapped(value)
+      #   api_matching_board = self.get_board_api_mapped(value)
         channels = res_json['result']['channel']['frameworks']
         for channel in channels:
          board_list.append(channel['identifier'])
 
-        return board_list, res_json
+        return board_list
 
      def medium_grade_db(self, medium):
         medium_list = []
@@ -201,60 +205,52 @@ class ActionContentForm(FormAction):
         grade_list = []
         global medium_grade_categories
     
-        global framework_response
-
         a = FrameworkApi()
         
-        print("a.channel_data-->",a.framework_data)
+      #   print("a.channel_data-->",a.framework_data)
 
         if not bool(a.framework_data):
            grade_medium_api_response = a.fetch_framework_data(board_identifier)
-           print("method3-->",grade_medium_api_response)
+         #   print("method3-->",grade_medium_api_response)
         else:
            cached_mediums = a.framework_data.keys()
-           print("cached_mediums-->",cached_mediums)
-           print("comparing value in cached_mediums",board_identifier)
+         #   print("cached_mediums-->",cached_mediums)
+         #   print("comparing value in cached_mediums",board_identifier)
            if board_identifier in cached_mediums :
             #   
               grade_medium_api_response = a.check_ttl_get_data("","",a.framework_data,board_identifier)
-              print("grade_medium_api_response method2-->",grade_medium_api_response)
+            #   print("grade_medium_api_response method2-->",grade_medium_api_response)
               
            else:
               grade_medium_api_response = a.fetch_framework_data(board_identifier)
-              print("grade_medium_api_response method1-->",grade_medium_api_response)  
-
-
-
-      #   grade_mdium_url = "https://staging.ntp.net.in/api/framework/v1/read/" + \
-      #       board_identifier + "?categories=board,medium,gradeLevel,subject"
-      #   grade_medium_api_response = fetch_framework_data(board_identifier)
-      #   grade_medium_api_response = requests.get(grade_mdium_url)
+            #   print("grade_medium_api_response method1-->",grade_medium_api_response)  
         res_framwork = grade_medium_api_response.json()
         medium_grade_categories = res_framwork['result']['framework']['categories']
+        medium_to_compare = ''
+        if medium != '':
+           medium_to_compare = self.get_medium_mapped(medium.lower())
 
-        medium_to_compare = self.get_medium_mapped(medium.lower())
-        print("medium_to_compare-->", medium_to_compare)
+        
+      #   print("medium_to_compare-->", medium_to_compare)
 
         for category in medium_grade_categories:
               if "medium" in category["code"]:
                  for term in category["terms"]:
                     medium_list.append(term['name'])
-                    if medium_to_compare in term["name"]:
-                       medium_available = True
+                    if medium_to_compare != '' and medium_to_compare in term["name"]:
                        for association in term["associations"]:
                           if association["category"] == "gradeLevel":
                              grade_list.append(association['description'])
 
-                       print("medium matched-->", medium_available)
                        break
         return medium_list
 
      def grade_db(self, grade):
 
-        grade_to_compare = self.get_grade_mapped(grade.lower())
-        print("grade_to_compare-->", grade_to_compare)
+      #   grade_to_compare = self.get_grade_mapped(grade.lower())
+      #   print("grade_to_compare-->", grade_to_compare)
 
-        print("grade_list in grade_db-->", grade_list)
+      #   print("grade_list in grade_db-->", grade_list)
         return grade_list
 
      def validate_board(self,
@@ -265,22 +261,31 @@ class ActionContentForm(FormAction):
 
         global board_identifier
         redisSessionData ['board'] = value
-      #   redisSlot =  { "board":value }
         redisSlot =  json.dumps(redisSessionData)
         self.setRedisKeyValue(tracker.sender_id, redisSlot)
-        print("tracker --> ", dir(tracker))
-        deviceId = tracker.sender_id
-        print("sender --> ", deviceId , type(deviceId) ," and ", type(str(deviceId)))
-      #   print("value in validate_board -->",value)
-      #   setRedisKeyValue(tracker.sender_id, value)
-        board_list, res_json = self.board_db(value)
+  
+        print("calling board_db from validate_board")
+        board_list = self.board_db(value)
+        print("board_list-->",board_list)
         api_matching_board = self.get_board_api_mapped(value.lower())
         if(api_matching_board in board_list):
            board_identifier = api_matching_board
-         #   print("calling redis in if for ",tracker.sender_id," and ", str(tracker.sender_id))
-         #   self.setRedisKeyValue(tracker.sender_id, value)
-         #   redisValue = redisClient.get("action_"+ str(deviceId))
-         #   print("redis get value", redisValue , " and ",type(redisValue))
+           medium_list = self.medium_grade_db('')
+           medium_buttons = []
+           for medium in medium_list:
+              medium_buttons.append({"text": medium, "value": medium})
+              print("medium_buttons-->", medium_buttons)
+
+           elements = [{
+               "blocks": [{
+                   "intent": "greet",
+                   "text": "Please choose the medium  <br>",
+                   "buttons": medium_buttons,
+                   "type": "response"
+               }]
+           }]
+
+           dispatcher.utter_message(json_message=elements)
            return value
         else:
            reafactored_board_list = []
@@ -293,16 +298,13 @@ class ActionContentForm(FormAction):
            elements = [{
                "blocks": [{
                    "intent": "greet",
-                   "text": "Available boards are : <br> ",
+                   "text": "Sorry ! Available boards are : <br> ",
                    "buttons": board_buttons,
                    "type": "response"
                }]
            }]
           
            dispatcher.utter_message(json_message=elements)
-         #   print("calling redis in else")
-         #   self.setRedisKeyValue(tracker.sender_id, elements)
-         #   print("redis get value"+redisClient.get("action_"+ tracker.sender_id))
            return None
 
      def validate_medium(self,
@@ -318,17 +320,31 @@ class ActionContentForm(FormAction):
         medium_list = self.medium_grade_db(value)
         api_matching_medium = self.get_medium_mapped(value.lower())
         if (api_matching_medium in medium_list):
+           grade_buttons = []
+           for grade in grade_list:
+              grade_buttons.append({"text": grade, "value": grade})
+         #   print("grade_buttons-->", grade_buttons)
+           print("tracker.get_slot('medium') -->",tracker.get_slot('medium'))
+           elements = [{
+               "blocks": [{
+                   "intent": "greet",
+                   "text": "Please choose the grade  <br> ",
+                   "buttons": grade_buttons,
+                   "type": "response"
+               }]
+           }]
+           dispatcher.utter_message(json_message=elements)
            return value
         else:
            medium_buttons = []
            for medium in medium_list:
               medium_buttons.append({"text": medium, "value": medium})
-           print("medium_buttons-->", medium_buttons)
+         #   print("medium_buttons-->", medium_buttons)
 
            elements = [{
                "blocks": [{
                    "intent": "greet",
-                   "text": "Available mediums for {board} board are : <br>".format(board=tracker.get_slot('board')),
+                   "text": "Sorry ! Available mediums for {board} board are : <br>".format(board=tracker.get_slot('board')),
                    "buttons": medium_buttons,
                    "type": "response"
                }]
@@ -355,7 +371,6 @@ class ActionContentForm(FormAction):
            grade_buttons = []
            for grade in grade_list:
               grade_buttons.append({"text": grade, "value": grade})
-           print("grade_buttons-->", grade_buttons)
            elements = [{
                "blocks": [{
                    "intent": "greet",
@@ -375,13 +390,13 @@ class ActionContentForm(FormAction):
         print("inside content_form")
         deviceId = tracker.sender_id
         redisValue = redisClient.get("action_"+ str(deviceId))
-        print("redisValue-->", redisValue , " and type --> ", type(redisValue))
+      #   print("redisValue-->", redisValue , " and type --> ", type(redisValue))
         redisValue_str = redisValue.decode('utf-8')
-        print("redisValue_str-->", redisValue_str," and type --> ", type(redisValue_str))
+      #   print("redisValue_str-->", redisValue_str," and type --> ", type(redisValue_str))
 
         redisTracker = json.loads(redisValue_str)
-        print("redisTracker -->",redisTracker, " type ",type(redisTracker)," and dir -->", dir(redisTracker))
-        print("redisTracker['board'] -->",redisTracker['board'])
+      #   print("redisTracker -->",redisTracker, " type ",type(redisTracker)," and dir -->", dir(redisTracker))
+      #   print("redisTracker['board'] -->",redisTracker['board'])
       
 
       #   board = tracker.get_slot('board')
