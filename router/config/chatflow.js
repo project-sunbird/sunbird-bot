bbdd831333f0162b232b14796f18c325136d6cbc
JagadishPujari <jagadish.pujari@tarento.com>
2020-12-09 16:02:05 +0530

    Issue #SB-21562 feat: Whatsapp>>As a user, I want to access Tara on whatsapp, So that i can get the required assistance

--- router/config/chatflow.js
+++ router/config/chatflow.js
@@ -189,117 +189,117 @@ var chatflow = {
     },
     step1_2_3_1: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (tamil nadu)",name: "TAMILNADU"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (tamil nadu)",name: "TAMILNADU", channel: "01235953109336064029450", slug: "tn"}},
         responseVariable: ""
     },
     step1_2_3_2: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (karnataka)",name: "KARNATAKA"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (karnataka)",name: "KARNATAKA", channel: "01248978648941363234", slug: "ka"}},
         responseVariable: ""
     },
     step1_2_3_3: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (gujarat)",name: "GUJARAT"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (gujarat)",name: "GUJARAT", channel: "01260242422044262448", slug: "gj"}},
         responseVariable: ""
     },
     step1_2_3_4: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (uttar pradesh)",name: "UTTAR PRADESH"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (uttar pradesh)",name: "UTTAR PRADESH", channel: "", slug: "up"}},
         responseVariable: ""
     },
     step1_2_3_5: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (punjab)",name: "PUNJAB"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (punjab)",name: "PUNJAB", channel: "01259891801536921611", slug: "pb"}},
         responseVariable: ""
     },
     step1_2_3_6: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (rajasthan)",name: "RAJASTAN"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (rajasthan)",name: "RAJASTAN", channel: "01246375399411712074", slug: "rj"}},
         responseVariable: ""
     },
     step1_2_3_7: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (manipur)",name: "MANIPUR"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (manipur)",name: "MANIPUR", channel: "01260243155748454421", slug: "mn"}},
         responseVariable: ""
     },
     step1_2_3_8: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (chhattisgarh)",name: "CHHATTISGARH"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (chhattisgarh)",name: "CHHATTISGARH", channel: "012361885070827520110898", slug: "ct"}},
         responseVariable: ""
     },
     step1_2_3_9: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (maharashtra)",name: "MAHARASTHTRA"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (maharashtra)",name: "MAHARASTHTRA", channel: "0123221617357783046602", slug: "mitra"}},
         responseVariable: ""
     },
     step1_2_3_10: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (bihar)",name: "BIHAR"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (bihar)",name: "BIHAR", channel: "012593397993308160261", slug: "br"}},
         responseVariable: ""
     },
     step1_2_3_11: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (odisha)",name: "ODISHA"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (odisha)",name: "ODISHA", channel: "012582683533344768108", slug: "od"}},
         responseVariable: ""
     },
     step1_2_3_12: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (assam)",name: "ASSAM"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (assam)",name: "ASSAM", channel: "01254290140407398431", slug: "as"}},
         responseVariable: ""
     },
     step1_2_3_13: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (madhya pradesh)",name: "MADHYA PRADESH"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (madhya pradesh)",name: "MADHYA PRADESH", channel: "012936530816278528135", slug: "mp"}},
         responseVariable: ""
     },
     step1_2_3_14: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (haryana)",name: "HARYANA"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (haryana)",name: "HARYANA", channel: "01257280950159769620", slug: "hr"}},
         responseVariable: ""
     },
     step1_2_3_15: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (nagaland)",name: "NAGALAND"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (nagaland)",name: "NAGALAND", channel: "012585429003968512109", slug: "nl"}},
         responseVariable: ""
     },
     step1_2_3_16: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (goa)",name: "GOA"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (goa)",name: "GOA", channel: "0126830867681034247", slug: "ga"}},
         responseVariable: ""
     },
     step1_2_3_17: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (telangana)",name: "TELENGANA"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (telangana)",name: "TELANGANA", channel: "0126640793823641603", slug: "ts"}},
         responseVariable: ""
     },
     step1_2_3_18: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (andhra pradesh)",name: "ANDHRA PRADESH"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (andhra pradesh)",name: "ANDHRA PRADESH", channel: "0123207707019919361056", slug: "apekx"}},
         responseVariable: ""
     },
     step1_2_3_19: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (meghalaya)",name: "MEGHALAYA"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (meghalaya)",name: "MEGHALAYA", channel: "012837368918761472986", slug: "ml"}},
         responseVariable: ""
     },
     step1_2_3_20: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "NCERT",name: "NCERT"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "NCERT",name: "NCERT", channel: "0125196274181898243", slug: "ncert"}},
         responseVariable: ""
     },
     step1_2_3_21: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (jharkhand)",name: "JHARKHAND"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (jharkhand)",name: "JHARKHAND", channel: "01263350230999859261", slug: "jh"}},
         responseVariable: ""
     },
     step1_2_3_22: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (sikkim)",name: "SIKKIM"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (sikkim)",name: "SIKKIM", channel: "0126002713272729607", slug: "sk"}},
         responseVariable: ""
     },
     step1_2_3_23: {
         messageKey: "NISHTHA_BOARD",
-        data: {replaceLabels: {key: "Nishtha 2020", board: "state (chandigarh)",name: "CHANDIGARH"}},
+        data: {replaceLabels: {key: "Nishtha2020", board: "state (chandigarh)",name: "CHANDIGARH", channel: "0125805318613565447", slug: "ch"}},
         responseVariable: ""
     },
     step1_2_2_error: {
